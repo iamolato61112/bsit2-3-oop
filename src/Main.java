bfc9110d54828d9e7d5e470d56980daf8c699734
Iniego Amolato <iamolato61112@liceo.edu.ph>
2025-08-03 01:09:29 +0800

    lab-activity-2.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Test Branch!");
+
+	public static void main(String[] args){
+    	Scanner scanner = new Scanner (System.in);
+    	
+    	 int totalNumber = 0;
+        int count = 1;
+    
+    	while (count <= 5) {
+    	    System.out.print("Enter a number:");
+    	    System.out.println(totalNumber + " ");
+            int Number = scanner.nextInt();
+            totalNumber += Number;
+            count ++;
+       		}
+        	System.out.println("\nTotal Number" + totalNumber);
+        
+    	scanner.close();
     }
 }
