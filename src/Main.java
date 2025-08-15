a0fb474342bc3c4643eb02bbba0d4948c66b302a
Iniego Amolato <iamolato61112@liceo.edu.ph>
2025-08-15 22:05:14 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main (String[] args){
-        System.out.println("Test Branch!");
-    }
+public static void main(String[] args) {
+
+    GradeCalculator gradeCalculator = new GradeCalculator();
+    String studentName = "John Smith";
+    double average = gradeCalculator.calculateAverage(85.5, 92.0, 78.5, 90.0);
+    String letterGrade = gradeCalculator.getLetterGrade(average);
+
+    gradeCalculator.displayResult(studentName, average);
+    gradeCalculator.displayResult(studentName, average, letterGrade);
 }
