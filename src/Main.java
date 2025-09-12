eee223f46487c1ef3308c7a7886827fbe485d706
Iniego Amolato <iamolato61112@liceo.edu.ph>
2025-09-12 19:49:27 +0800

    lab-activity-8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Test Branch!");
+    public static void main(String[] args) {
+
+        Manager manager = new Manager("Alice Smith", 2001, 80000, "Engineering", 15000, 8);
+        System.out.println();
+        Developer developer = new Developer("Bob Johnson", 2002, 70000, "Engineering", "Java", 5);
+        System.out.println();
+        Intern intern = new Intern("Charlie Brown", 2003, 30000, "Engineering", "Tech University", true);
+        System.out.println();
+
+
+        System.out.println("\n*~~~~Employee Details~~~~*");
+        System.out.println("Manager Info:");
+        manager.displayInfo();
+        manager.work();
+        System.out.println("Monthly Salary: $" + manager.calculateSalary());
+        System.out.println();
+
+        System.out.println("\n*~~~~Employee Details~~~~*");
+        System.out.println("Developer Info:");
+        developer.displayInfo();
+        developer.work();
+        System.out.println("Monthly Salary: $" + developer.calculateSalary());
+        System.out.println();
+
+        System.out.println("\n*~~~~Employee Details~~~~*");
+        System.out.println("Intern Info:");
+        intern.displayInfo();
+        intern.work();
+        System.out.println("Monthly Salary: $" + intern.calculateSalary());
     }
 }
